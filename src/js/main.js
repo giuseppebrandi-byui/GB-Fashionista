d7ca73db5890d930bf735e03f9c3847ea0be99de
giuseppebrandi-byui <bra22041@byui.edu>
2023-11-20 19:23:43 +0000

    Testing the baseUrl

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
+const baseURL = import.meta.env.VITE_SERVER_URL;
 import { showHeroImage } from "./main.mjs";
 
 showHeroImage();
+console.log("This is the baseUrl: ", baseURL);
+console.log("This is process.env: ", process.env.VITE_SERVER_URL);
