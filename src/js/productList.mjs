3935f5c1667a1810c72734624f3ae38fd9eb9dc7
giuseppebrandi-byui <bra22041@byui.edu>
2023-11-23 09:57:13 +0000

    Added renderListWithTemplate function

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -1,7 +1,37 @@
 import { getProductByCategory } from "./productData.mjs";
 
 function productCardTemplate(product) {
-  return ``;
+  return `<div class="product-card">
+            <a
+              href="product_pages/index.html?category=${product.category}&&product=${product.id}"
+            >
+              <img
+                class="product-img"
+                src="${product.image}"
+                alt="${product.title}"
+              />
+              <h2 class="card__name">${product.title}</h2>
+              <p class="product-card__price">$${product.price}</p></a
+            >
+          </div>`;
+}
+
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = true
+) {
+  if (clear && parentElement !== null) {
+    parentElement.innerHTML = "";
+  }
+  console.log("This is a great list: ", list);
+  const htmlString = list.map(templateFn);
+
+  if (parentElement !== null) {
+    parentElement.insertAdjacentHTML(position, htmlString.join(""));
+  }
 }
 
 export default async function product_list(selector, category) {
