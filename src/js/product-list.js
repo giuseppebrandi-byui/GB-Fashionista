62fbc70c7a8ac1aeefcacb50d562940df70e4e83
giuseppebrandi-byui <bra22041@byui.edu>
2023-11-23 10:18:56 +0000

    Added logic to display products by category

--- src/js/product-list.js
+++ src/js/product-list.js
@@ -1,1 +1,5 @@
+import { loadHeaderFooter, getParam } from "./utils.mjs";
 import productList from "./productList.mjs";
+loadHeaderFooter();
+const category = getParam("category");
+productList(".product-fullList", category);
