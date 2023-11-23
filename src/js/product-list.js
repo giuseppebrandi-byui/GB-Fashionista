079fe55aca91a25d84f0c24849f100268a51141f
giuseppebrandi-byui <bra22041@byui.edu>
2023-11-23 13:15:07 +0000

    Added product list with and without categories

--- src/js/product-list.js
+++ src/js/product-list.js
@@ -1,10 +1,9 @@
+import { loadHeaderFooter, getParam } from "./utils.mjs";
+import productList from "./productList.mjs";
 // import { loadHeaderFooter, getParam } from "./utils.mjs";
-// import productList from "./productList.mjs";
 // loadHeaderFooter();
-// const category = getParam("category");
-// productList(".product-fullList", category);
+const category = getParam("category");
+console.log("Testing category: " + category);
+productList(".product-fullList", category);
 
-import { loadHeaderFooter } from "./utils.mjs";
-import getAllProducts from "./productListAll.mjs";
 loadHeaderFooter();
-getAllProducts();
--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -1,10 +1,11 @@
 import { getProductByCategory } from "./productData.mjs";
 import { renderListWithTemplate } from "./utils.mjs";
 
+let products;
 function productCardTemplate(product) {
   return `<div class="product-card">
             <a
-              href="product_pages/index.html?category=${product.category}&&product=${product.id}"
+              href="/product_pages/index.html?category=${product.category}&&product=${product.id}"
             >
               <img
                 class="product-img"
@@ -17,11 +18,23 @@ function productCardTemplate(product) {
           </div>`;
 }
 
-export default async function product_list(selector, category) {
+export default async function product_list(selector, category = "all") {
   // get the element we will insert the list into from the selector
   const el = document.querySelector(selector);
-  // get the list of products
-  const products = await getProductByCategory(category);
+  console.log("This is category: ", category);
+  if (category == "all" || category == "null" || category == undefined) {
+    const menClothing = await getProductByCategory("men's clothing");
+    const jewelery = await getProductByCategory("jewelery");
+    const womenClothing = await getProductByCategory("women's clothing");
+
+    products = menClothing.concat(jewelery, womenClothing);
+  } else {
+    // get the list of products
+    products = await getProductByCategory(category);
+    document.querySelector(".dynamic-category-heading").textContent =
+      category.charAt(0).toUpperCase() + category.slice(1);
+  }
+
   // render out the product list to the element
   renderListWithTemplate(productCardTemplate, el, products);
 }
