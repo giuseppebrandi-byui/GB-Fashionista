fde1348511e84170a4b7b338b67c04fb853f57d5
giuseppebrandi-byui <bra22041@byui.edu>
2023-11-24 23:03:43 +0000

    Added HTML and CSS for cart layout

--- src/js/cart.js
+++ src/js/cart.js
@@ -7,21 +7,35 @@ function renderCartContents() {
 }
 
 function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card">
-  <a href="/product_pages/index.html?category=${item.category}&&product=${item.id}" class="cart-card__image">
-    <img
-      src="${item.image}"
-      alt="${item.title}"
-    />
-  </a>
-  <a href="/product_pages/index.html?category=${item.category}&&product=${item.id}">
-    <h2 class="card__name">${item.title}</h2>
-  </a>
-  <p class="cart-card__quantity">qty: ${item.productOnHold}</p>
-  <p class="cart-card__price">$${item.price}</p>
-</li>`;
+  let subtotal = (item.price * item.productOnHold).toFixed(2);
+  const newItems = `
+            <tr class="basket-product">
+              <td>
+                <a href="#"><ion-icon class="remove-product" name="trash-outline"></ion-icon></a>
+              </td>
+              <td>
+                <a href="/product_pages/index.html?category=${
+                  item.category
+                }&&product=${item.id}" class="cart-card__image">
+                  <img
+                    src="${item.image}"
+                    alt="${item.title}"
+                  />
+                </a>
+              </td>
+              <td>
+                <a href="/product_pages/index.html?category=${
+                  item.category
+                }&&product=${item.id}">
+                  <h2 class="card__name">${item.title}</h2>
+                </a>
+              </td>
+              <td>$${item.price.toFixed(2)}</td>
+              <td><input type="number" value=${item.productOnHold} /></td>
+              <td>$${subtotal}</td>
+            </tr>`;
 
-  return newItem;
+  return newItems;
 }
 
 renderCartContents();
--- src/js/product-list.js
+++ src/js/product-list.js
@@ -1,9 +1,7 @@
 import { loadHeaderFooter, getParam } from "./utils.mjs";
 import productList from "./productList.mjs";
-// import { loadHeaderFooter, getParam } from "./utils.mjs";
-// loadHeaderFooter();
+
 const category = getParam("category");
-console.log("Testing category: " + category);
 productList(".product-fullList", category);
 
 loadHeaderFooter();
