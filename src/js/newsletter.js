05d4934e4667fbeac7ef86cd71f0afaf43c2aaf3
giuseppebrandi-byui <bra22041@byui.edu>
2023-12-09 08:45:27 +0000

    Added email validation to newsletter

--- src/js/newsletter.js
+++ src/js/newsletter.js
@@ -6,12 +6,21 @@ const modal = document.querySelector("#newsLetterModal");
 const newsLetterBtn = document.querySelector(".newsletterBtn");
 // Get the span element that closes the modal
 const newsLetterCloseBtn = document.querySelector(".close");
+// Get the p element with the thank you message
+const newsletterMessage = document.querySelector(".newsletter-thanks");
 
 // When the user clicks the button, open the modal
 newsLetterBtn.onclick = function (e) {
   e.preventDefault();
+  let validRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   let emailFieldValue = document.querySelector("[name=email]").value;
-  if (emailFieldValue !== "") {
+  if (emailFieldValue !== "" && emailFieldValue.match(validRegex)) {
+    newsletterMessage.textContent =
+      "Thank you for subscribing to our newsletter";
+    modal.style.display = "block";
+  } else {
+    newsletterMessage.textContent = "Please enter a valid email";
     modal.style.display = "block";
   }
 };
